7475fc461c7f8fa5bd217a8a015741133bb60970
samno1 <samno1@yandex.ru>
2016-12-28 02:48:26 +0300

    javarush

--- src/com/javarush/test/level04/lesson13/task04/Solution.java
+++ src/com/javarush/test/level04/lesson13/task04/Solution.java
@@ -13,9 +13,19 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
         //напишите тут ваш код
-
-
-
-
+        for (int i=0; i < 12; i++) {
+            if(i==0) {
+            for(int j=0;j<10;j++) {
+                System.out.print("8");
+            }
+        }
+        if (i==2) {
+            System.out.println(" ");
+        }
+        if (i>=2)
+        {
+            System.out.println("8");
+        }
+        }
     }
 }
